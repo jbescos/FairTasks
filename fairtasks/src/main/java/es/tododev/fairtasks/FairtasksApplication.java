968973b0f8068139461d40a5b44cd95087f5b483
jbescos <jorgebescos@gmail.com>
2018-11-11 14:24:17 +0100

    Working with spring boot

---
+++ fairtasks/src/main/java/es/tododev/fairtasks/FairtasksApplication.java
@@ -0,0 +1,12 @@
+package es.tododev.fairtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairtasksApplication.class, args);
+	}
+}
