1ffb8659b5d97ea063e213beff6b8579f22f7fee
jbescos <jorgebescos@gmail.com>
2018-11-12 19:36:41 +0100

    Adding data

--- fairtasks/src/test/resources/data.sql
+++ fairtasks/src/test/resources/data.sql
@@ -1,1 +1,11 @@
-insert into users(EMAIL, USER_NAME) values('test@test.com','test');
+insert into USERS(EMAIL, USER_NAME) values('boy@test.com','boy');
+insert into USERS(EMAIL, USER_NAME) values('girl@test.com','girl');
+
+insert into GROUPS(EMAIL, GROUP_NAME) values('test@test.com','home');
+
+insert into TASKS(TASK, GROUP_NAME, VALUE, DESCRIPTION) values('Wash dishes','home',100,null);
+insert into TASKS(TASK, GROUP_NAME, VALUE, DESCRIPTION) values('Drop garbage','home',50,null);
+insert into TASKS(TASK, GROUP_NAME, VALUE, DESCRIPTION) values('Cooking','home',150,null);
+insert into TASKS(TASK, GROUP_NAME, VALUE, DESCRIPTION) values('Vacuum cleaner','home',110,null);
+insert into TASKS(TASK, GROUP_NAME, VALUE, DESCRIPTION) values('Shopping','home',200,null);
+insert into TASKS(TASK, GROUP_NAME, VALUE, DESCRIPTION) values('Clean bathroom','home',300,null);
